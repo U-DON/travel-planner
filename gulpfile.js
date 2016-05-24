b6edf87e66993a35af309f30db01e8cacc83635f
Eugene Doan <doan.eugene@gmail.com>
2016-05-24 18:22:07 -0400

    Updated gulpfile. Added clean task, sync upon any HTML, CSS, JS change, and CSS minification.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,57 @@
 var autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    cleanCss = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    del = require('del'),
     gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify');
 
+gulp.task('clean', function () {
+    del(['public/css/*', 'public/js/*']);
+});
+
 gulp.task('css', function () {
     gulp.src(['assets/css/**/*.scss'])
         .pipe(plumber())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 2 versions'))
-        .pipe(gulp.dest('public/css'));
+        .pipe(cleanCss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('js', function () {
     gulp.src(['assets/js/**/*.js', '!assets/js/**/*.min.js'])
-        .pipe(rename({suffix: '.min'}))
+        .pipe(plumber())
+        .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('public/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('html', function () {
+    gulp.src(['*.html'])
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sync', function () {
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
 });
 
 gulp.task('watch', function () {
     gulp.watch('assets/css/**/*.scss', ['css']);
     gulp.watch('assets/js/**/*.js', ['js']);
+    gulp.watch('*.html', ['html']);
 
 });
 
-gulp.task('default', ['css', 'js', 'watch']);
+gulp.task('default', ['clean', 'css', 'js', 'sync', 'watch']);
