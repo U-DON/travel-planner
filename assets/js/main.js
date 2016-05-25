3eb188b911237a5cd0a43411cba5c281c8a08ecc
Eugene Doan <doan.eugene@gmail.com>
2016-05-25 19:41:36 -0400

    Restructured layout and some more functionality for mockup.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,13 @@ var initMap = function () {
         center: new google.maps.LatLng(37.09024, -95.712891),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         mapTypeControl: false,
-        zoom: 3
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.TOP_RIGHT
+        },
+        zoom: 3,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.TOP_RIGHT
+        }
     };
 
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -27,12 +33,19 @@ var initMap = function () {
 
     map.addListener('click', function () {
         infoWindow.close();
-        selectionDiv.html("");
+        selectionDiv.hide();
     });
 
     var places = new google.maps.places.PlacesService(map);
 
-    var searchBox = new google.maps.places.SearchBox(document.getElementById('search'));
+    var searchControl = document.getElementById('search-control');
+    var searchInput = document.getElementById('search-box');
+    var searchBox = new google.maps.places.SearchBox(searchInput);
+
+    var selectionControl = document.getElementById('selection-control');
+
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(searchControl);
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(selectionControl);
 
     searchBox.addListener('places_changed', function () {
         var places = searchBox.getPlaces();
@@ -56,12 +69,51 @@ var initMap = function () {
             })
 
             marker.addListener('click', function () {
+                console.log(place);
                 infoWindow.setContent("<h3>" + place.name + "</h3>"
                                       + place.formatted_address);
                 infoWindow.open(map, this);
-                selectionDiv.html("");
-                selectionDiv.append($("<h2>" + place.name + "</h2>"));
-                selectionDiv.append($("<p>" + place.formatted_address + "</p>"));
+
+                var selectionPhoto = $('#selection-photo');
+                var selectionInfo = $('#selection-info');
+                selectionPhoto.html("");
+                selectionInfo.html("");
+
+                if (place.photos) {
+                    var photo = place.photos[0];
+                    var photoUrl;
+
+                    // Scale photo according to its dimensions.
+                    if (photo.width > photo.height) {
+                        photoUrl = photo.getUrl({ maxHeight: 360 });
+                    } else {
+                        photoUrl = photo.getUrl({ maxWidth: 540 });
+                    }
+
+                    selectionPhoto.css('background-image', "url(" + photoUrl + ")");
+                } else {
+                    selectionPhoto.css('background-image', 'none');
+                }
+
+                selectionInfo.append($("<h2>" + place.name + "</h2>"));
+
+                if (place.formatted_address) {
+                    selectionInfo.append($("<p>" + place.formatted_address + "</p>"));
+                }
+
+                if (place.formatted_phone_number) {
+                    selectionInfo.append($("<p>" + place.formatted_phone_number + "</p>"));
+                }
+
+                if (place.price_level) {
+                    selectionInfo.append($("<p>" + "$".repeat(place.price_level) + "</p>"));
+                }
+
+                if (place.website) {
+                    selectionInfo.append($('<a />', { href: place.website, text: "Website" }));
+                }
+
+                selectionDiv.show();
             });
 
             // Create a marker for each place.
