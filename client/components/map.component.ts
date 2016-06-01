24599e56d57152b9ee6d98e08caf9fc5c9385e71
Eugene Doan <doan.eugene@gmail.com>
2016-06-01 19:29:40 -0400

    Place plan markers on map and differentiate them from place markers. Using SVG for marker icons.

--- client/components/map.component.ts
+++ client/components/map.component.ts
@@ -3,11 +3,51 @@ import {
     Component,
     ElementRef,
     NgZone,
+    OnDestroy,
     ViewChild
 } from "@angular/core";
 
+import { PlannerService } from "../services/planner.service";
+import { Plan } from "./planner.component";
 import { MapService } from "../services/map.service";
 import { SelectionComponent } from "./selection.component";
+
+namespace MapMarkerIcon {
+
+    export const MARKER = {
+        path: `
+            M768 896q0 106 -75 181t-181 75t-181 -75t-75 -181t75 -181t181
+            -75t181 75t75 181zM1024 896q0 -109 -33 -179l-364 -774q-16 -33 -47.5
+            -52t-67.5 -19t-67.5 19t-46.5 52l-365 774q-33 70 -33 179q0 212 150
+            362t362 150t362 -150t150 -362z
+        `,
+        fillColor: "#82ffeb",
+        fillOpacity: 1,
+        rotation: 180,
+        scale: 0.02,
+        strokeColor: "black",
+        strokeOpacity: 0.5,
+        strokeWeight: 1
+    };
+
+    export const STAR = {
+        path: `
+            M1664 889q0 -22 -26 -48l-363 -354l86 -500q1 -7 1 -20q0 -21 -10.5
+            -35.5t-30.5 -14.5q-19 0 -40 12l-449 236l-449 -236q-22 -12 -40
+            -12q-21 0 -31.5 14.5t-10.5 35.5q0 6 2 20l86 500l-364 354q-25 27 -25
+            48q0 37 56 46l502 73l225 455q19 41 49 41t49 -41l225 -455 l502
+            -73q56 -9 56 -46z
+        `,
+        fillColor: "#ff9c50",
+        fillOpacity: 1,
+        rotation: 180,
+        scale: 0.02,
+        strokeColor: "black",
+        strokeOpacity: 0.5,
+        strokeWeight: 1
+    };
+
+}
 
 export class PlaceInfo {
 
@@ -90,20 +130,41 @@ export class MapComponent implements AfterViewInit {
     @ViewChild("searchControl") searchControl: ElementRef;
     @ViewChild("searchBox") searchInput: ElementRef;
 
+    private _planAddedSubscription: any;
+    private _planRemovedSubscription: any;
+
     map: google.maps.Map;
     markers: google.maps.Marker[] = [];
+    planMarkers: Map<string, google.maps.Marker> = new Map<string, google.maps.Marker>();
     searchBox: google.maps.places.SearchBox;
     selection: PlaceInfo;
 
     constructor (private _zone: NgZone,
-                 private _mapService: MapService)
+                 private _mapService: MapService,
+                 private _plannerService: PlannerService)
     {
+        this._planAddedSubscription =
+            this._plannerService.planAdded.subscribe((plan: Plan) => {
+                this.createPlanMarker(plan);
+            });
+
+        this._planRemovedSubscription =
+            this._plannerService.planRemoved.subscribe((plan: Plan) => {
+                // Update selection to not display plan details; only place info.
+                // if (plan.place.placeId === this.selection.placeId) { }
+                this.planMarkers.get(plan.place.placeId).setMap(null);
+                this.planMarkers.delete(plan.place.placeId);
+            });
     }
 
     ngAfterViewInit () {
         this._mapService.initApi().then(() => {
             this.initMap();
         });
+    }
+
+    ngOnDestroy () {
+        this._planRemovedSubscription.unsubscribe();
     }
 
     initMap () {
@@ -153,10 +214,17 @@ export class MapComponent implements AfterViewInit {
         this.markers = [];
     }
 
-    createMarker (place: google.maps.places.PlaceResult) {
+    createPlaceMarker (place: google.maps.places.PlaceResult) {
+        // Correct the position of the icon.
+        // http://stackoverflow.com/a/32483737/1070621
         let marker = new google.maps.Marker({
+            icon: Object.assign({
+                    anchor: new google.maps.Point(512, 0)
+                }, MapMarkerIcon.MARKER),
             map: this.map,
+            opacity: 0.7,
             position: place.geometry.location,
+            zIndex: 1
         })
 
         marker.addListener("click", () => {
@@ -167,9 +235,96 @@ export class MapComponent implements AfterViewInit {
             });
         });
 
+        // Use another marker to indicate interaction with the main marker.
+        let tempMarker: google.maps.Marker;
+
+        marker.addListener("mouseover", () => {
+            marker.setOpacity(1);
+            marker.setZIndex(1000);
+            tempMarker = new google.maps.Marker({
+                icon: {
+                    anchor: new google.maps.Point(0, 0.5),
+                    path: google.maps.SymbolPath.CIRCLE,
+                    fillColor: "transparent",
+                    strokeColor: "#ffffff",
+                    strokeOpacity: 1,
+                    strokeWeight: 2,
+                    scale: 25
+                },
+                map: this.map,
+                position: place.geometry.location,
+                zIndex: 999
+            });
+        });
+
+        marker.addListener("mouseout", () => {
+            marker.setOpacity(0.7);
+            marker.setZIndex(100);
+            tempMarker.setMap(null);
+        });
+
         this.markers.push(marker);
 
         return marker;
+    }
+
+    createPlanMarker (plan: Plan) {
+        if (this.planMarkers.has(planplace.placeId))
+            return;
+
+        let marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP,
+            // Correct the position of the icon.
+            // http://stackoverflow.com/a/32483737/1070621
+            icon: Object.assign({
+                    anchor: new google.maps.Point(832, 0)
+                }, MapMarkerIcon.STAR),
+            map: this.map,
+            opacity: 0.7,
+            position: plan.place.geometry.location,
+            zIndex: 100
+        });
+
+        // Use another marker to indicate interaction with the main marker.
+        let tempMarker: google.maps.Marker;
+
+        marker.addListener("click", () => {
+            this.searchInput.nativeElement.blur();
+            this.map.panTo(plan.place.geometry.location);
+            this._zone.run(() => {
+                this.selection = plan.place;
+            });
+        });
+
+        marker.addListener("mouseover", () => {
+            marker.setOpacity(1);
+            marker.setZIndex(1000);
+            tempMarker = new google.maps.Marker({
+                icon: {
+                    anchor: new google.maps.Point(0, 0.5),
+                    path: google.maps.SymbolPath.CIRCLE,
+                    fillColor: "transparent",
+                    strokeColor: "#ffffff",
+                    strokeOpacity: 1,
+                    strokeWeight: 2,
+                    scale: 25
+                },
+                map: this.map,
+                position: plan.place.geometry.location,
+                zIndex: 999
+            });
+        });
+
+        marker.addListener("mouseout", () => {
+            marker.setOpacity(0.7);
+            marker.setZIndex(100);
+            tempMarker.setMap(null);
+        });
+
+        this.planMarkers.set(plan.place.placeId, marker);
+
+        return marker;
+
     }
 
     onPlacesChanged () {
@@ -185,7 +340,7 @@ export class MapComponent implements AfterViewInit {
 
         places.forEach((place: google.maps.places.PlaceResult) => {
             // Create a marker for each place.
-            this.createMarker(place);
+            this.createPlaceMarker(place);
 
             // Accommodate this place in the current map view.
             if (place.geometry.viewport) {
