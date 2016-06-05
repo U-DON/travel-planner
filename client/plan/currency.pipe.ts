334353dace7519378ed1f4d86d65d5042361e948
Eugene Doan <doan.eugene@gmail.com>
2016-06-05 15:59:35 -0400

    Don't transform currency and rating if they don't exist.

--- client/plan/currency.pipe.ts
+++ client/plan/currency.pipe.ts
@@ -5,12 +5,14 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class CurrencyPipe implements PipeTransform {
     transform (priceLevel: number): string {
-        let currencyText = "<i class='fa fa-usd'></i>".repeat(priceLevel)
-                         + "<span class='sr-only'>"
-                         + "Price Level: "
-                         + priceLevel
-                         + "</span>";
+        if (priceLevel) {
+            let currencyText = "<i class='fa fa-usd'></i>".repeat(priceLevel)
+                             + "<span class='sr-only'>"
+                             + "Price Level: "
+                             + priceLevel
+                             + "</span>";
 
-        return currencyText;
+            return currencyText;
+        }
     }
 }
--- client/plan/rating.pipe.ts
+++ client/plan/rating.pipe.ts
@@ -5,18 +5,20 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class RatingPipe implements PipeTransform {
     transform (rating: number): string {
-        let stars = Math.floor(rating);
-        let halfStar = (rating === stars ? 0 : 1);
-        let starRating = "<span title='"
-                       + rating
-                       + "'>"
-                       + "<i class='fa fa-star'></i>".repeat(stars)
-                       + "<i class='fa fa-star-half'></i>".repeat(halfStar)
-                       + "<span class='sr-only'>"
-                       + "Rating: "
-                       + rating
-                       + " stars</span></span>"
+        if (rating) {
+            let stars = Math.floor(rating);
+            let halfStar = (rating === stars ? 0 : 1);
+            let starRating = "<span title='"
+                           + rating
+                           + "'>"
+                           + "<i class='fa fa-star'></i>".repeat(stars)
+                           + "<i class='fa fa-star-half'></i>".repeat(halfStar)
+                           + "<span class='sr-only'>"
+                           + "Rating: "
+                           + rating
+                           + " stars</span></span>"
 
-        return starRating;
+            return starRating;
+        }
     }
 }
