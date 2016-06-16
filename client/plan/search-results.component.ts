16e95b98c52170c218b5bfc9770aafe95aa1605e
Eugene Doan <doan.eugene@gmail.com>
2016-06-16 18:46:48 -0400

    Styles for search results.

--- client/plan/search-results.component.ts
+++ client/plan/search-results.component.ts
@@ -6,10 +6,12 @@ import {
 import { Observable } from "rxjs";
 
 import { MapService } from "../map/map.service";
+import { CurrencyPipe, RatingPipe } from "./pipes";
 
 @Component({
     selector: "search-results",
     changeDetection: ChangeDetectionStrategy.OnPush,
+    pipes: [CurrencyPipe, RatingPipe],
     styles: [`
         :host {
             flex: 1;
@@ -18,11 +20,15 @@ import { MapService } from "../map/map.service";
     `],
     template: `
         <div id="search-results">
-            <div *ngFor="let result of searchResults | async">
-                <p>{{ result.name }}</p>
-                <p>{{ result.address }}</p>
-                <p>{{ result.rating }}</p>
-                <p>{{ result.price_level }}</p>
+            <div *ngFor="let result of searchResults | async" class="search-result">
+                <div class="plan-place">{{ result.name }}</div>
+                <div class="plan-summary">
+                    <div *ngIf="result.rating || result.priceLevel" class="plan-detail-group">
+                        <span *ngIf="result.rating" [innerHTML]="result.rating | toRating" class="plan-detail plan-rating"></span>
+                        <span *ngIf="result.price_level" [innerHTML]="result.price_level | toCurrency" class="plan-detail plan-price"></span>
+                    </div>
+                    <div class="plan-detail">{{ result.formatted_address }}</div>
+                </div>
             </div>
         </div>
     `
